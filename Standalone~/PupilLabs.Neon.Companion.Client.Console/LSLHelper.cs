677be0f406f8de7143d87004657e27ed4c53246f
ppedro74 <tiago.private@gmail.com>
2026-06-29 20:40:19 -0400

    .

---
+++ Standalone~/PupilLabs.Neon.Companion.Client.Console/LSLHelper.cs
@@ -0,0 +1,104 @@
+﻿namespace PupilLabs.Neon.Companion.Client.ConsoleApp
+{
+    using System.Diagnostics;
+    using SharpLSL;
+
+    internal static class LSLHelper
+    {
+        public static void GetLocalClock()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                var stopwatch = Stopwatch.StartNew();
+                var timestamp = LSL.GetLocalClock();
+                var elapsedTime = stopwatch.Elapsed;
+
+                stopwatch = Stopwatch.StartNew();
+                var timestamp2 = Stopwatch.GetTimestamp();
+                var elapsedTime2 = stopwatch.Elapsed;
+
+                Console.WriteLine($"{timestamp:F7} {elapsedTime} <- LSL.GetLocalClock()");
+                Console.WriteLine($"{TimestampToSeconds(timestamp2):F7} {elapsedTime2} <- TimestampToSeconds(Stopwatch.GetTimestamp())");
+            }
+
+            Console.WriteLine($"Is high resolution? {Stopwatch.IsHighResolution}.");
+            Console.WriteLine($"Ticks per second: {TimeSpan.TicksPerSecond}.");
+            Console.WriteLine($"Frequency: {Stopwatch.Frequency}.");
+            Console.WriteLine($"Tick frequency: {(double)TimeSpan.TicksPerSecond / Stopwatch.Frequency}.");
+        }
+
+        public static void ListAllStreams()
+        {
+            UnityEngine.Debug.Log("Listing all LSL streams...");
+
+            try
+            {
+                Console.WriteLine("Here is a one-shot resolve of all current streams:");
+
+                // Discover all streams on the network
+                var streamInfos = LSL.Resolve();
+
+                var foundStreams = new Dictionary<string, StreamInfo>();
+                foreach (var streamInfo in streamInfos)
+                {
+                    foundStreams[streamInfo.Uid] = streamInfo;
+                    Console.WriteLine(streamInfo.ToXML());
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Press any key to switch to the continuous resolver test.");
+                Console.ReadKey();
+
+                using (var continuousResolver = new ContinuousResolver())
+                {
+                    while (true)
+                    {
+                        var resolvedStreams = continuousResolver.Results();
+                        foreach (var streamInfo in resolvedStreams)
+                        {
+                            var streamUid = streamInfo.Uid;
+                            if (!foundStreams.ContainsKey(streamUid))
+                            {
+                                foundStreams[streamUid] = streamInfo;
+                                Console.WriteLine($"Found {streamInfo.Name}@{streamInfo.HostName}");
+                            }
+                        }
+
+                        var missingStreams = foundStreams.Values.Where(streamInfo => resolvedStreams.All(resolvedStreamInfo => resolvedStreamInfo.Uid != streamInfo.Uid));
+                        foreach (var streamInfo in missingStreams)
+                        {
+                            Console.WriteLine($"Lost {streamInfo.Name}@{streamInfo.HostName}");
+                            foundStreams.Remove(streamInfo.Uid);
+                        }
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Got an exception: {ex}");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        static double TimestampToSeconds(long timestamp)
+        {
+#if false
+            var tickFrequency = (double)TimeSpan.TicksPerSecond / Stopwatch.Frequency;
+            return timestamp * tickFrequency / TimeSpan.TicksPerSecond;
+#else
+            return (double)timestamp / Stopwatch.Frequency;
+#endif
+        }
+
+        static double TimestampToMilliseconds(long timestamp)
+        {
+            var tickFrequency = (double)TimeSpan.TicksPerSecond / Stopwatch.Frequency;
+            return timestamp * tickFrequency / TimeSpan.TicksPerMillisecond;
+        }
+
+
+    }
+}
--- Standalone~/PupilLabs.Neon.Companion.Client.Console/Program.cs
+++ Standalone~/PupilLabs.Neon.Companion.Client.Console/Program.cs
@@ -20,6 +20,9 @@
 
             [Option("time-echo-port", Required = false, HelpText = "Optional time echo port. If omitted, it is read from the Neon status.")]
             public int? TimeEchoPort { get; set; }
+
+            [Option("run-lsl", Required = false, Default = false, HelpText = "run lsl")]
+            public bool RunLsl { get; set; }
         }
 
         private static async Task Main(string[] args)
@@ -31,49 +34,58 @@
         {
             var neonIp = options.NeonIp;
             var neonPort = options.NeonPort;
-            var runGetStatus = options.RunGetStatus;
-            var runTimeEcho = options.RunTimeEcho;
             var timeEchoPort = options.TimeEchoPort;
 
-            if (!NetworkHelper.IsIpAddress(neonIp))
+            if (options.RunGetStatus || options.RunTimeEcho)
             {
-                var result = await NetworkHelper.ResolveHostToIpAsync(neonIp);
-                if (result.IsSuccess)
+                if (!NetworkHelper.IsIpAddress(neonIp))
                 {
-                    neonIp = result.Value;
+                    var result = await NetworkHelper.ResolveHostToIpAsync(neonIp);
+                    if (result.IsSuccess)
+                    {
+                        neonIp = result.Value;
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine($"Error: Failed to resolve neonIp=['{neonIp}'] ErrorMessage=[{result.ErrorMessage}]");
+                        return;
+                    }
                 }
-                else
+
+                if (options.RunGetStatus)
                 {
-                    Console.Error.WriteLine($"Error: Failed to resolve neonIp=['{neonIp}'] ErrorMessage=[{result.ErrorMessage}]");
-                    return;
+                    if (string.IsNullOrEmpty(neonIp))
+                    {
+                        Console.Error.WriteLine($"Error: NeonIp cannot be null.");
+                        return;
+                    }
+
+                    var (success, statusTimeEchoPort) = await NeonHelper.GetStatusAsync(neonIp, neonPort, new CancellationTokenSource(TimeSpan.FromSeconds(45)).Token);
+                    if (success && timeEchoPort == null)
+                    {
+                        timeEchoPort = statusTimeEchoPort;
+                    }
+                }
+
+                if (options.RunTimeEcho)
+                {
+                    if (timeEchoPort == null)
+                    {
+                        Console.WriteLine("Error: TimeEchoPort parameter not set.");
+                    }
+                    else
+                    {
+                        var result = await NeonHelper.GetEstimateTimeOffsetAsync(neonIp, timeEchoPort.Value, 25, 20, new CancellationTokenSource(TimeSpan.FromSeconds(45)).Token);
+                    }
                 }
             }
 
-            if (runGetStatus)
+            
+            if (options.RunLsl)
             {
-                if (string.IsNullOrEmpty(neonIp))
-                {
-                    Console.Error.WriteLine($"Error: NeonIp cannot be null.");
-                    return;
-                }
+                LSLHelper.GetLocalClock();
+                LSLHelper.ListAllStreams();
                 
-                var (success, statusTimeEchoPort) = await NeonHelper.GetStatusAsync(neonIp, neonPort, new CancellationTokenSource(TimeSpan.FromSeconds(45)).Token);
-                if (success && timeEchoPort == null)
-                {
-                    timeEchoPort = statusTimeEchoPort;
-                }
-            }
-
-            if (runTimeEcho)
-            {
-                if (timeEchoPort == null)
-                {
-                    Console.WriteLine("Error: TimeEchoPort parameter not set.");
-                }
-                else
-                {
-                    var result = await NeonHelper.GetEstimateTimeOffsetAsync(neonIp, timeEchoPort.Value, 25, 20, new CancellationTokenSource(TimeSpan.FromSeconds(45)).Token);
-                }
             }
         }
     }
